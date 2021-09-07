548d39a66e8bc7da518d8f8e9173fd52e0a33f7b
Sage Fiorentino-Lange <sage.fio.lange@gmail.com>
2021-09-07 14:48:08 -0600

    got logo working need to fix styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,13 @@
-import type { NextComponentType } from 'next'
+import type { NextComponentType } from "next";
 import Image from "next/image";
-import styles from '../styles/Header.module.css'
+import styles from "../styles/Header.module.css";
 
 const Header: NextComponentType = () => {
   return (
     <div className={styles.container}>
-      <Image src="../public/logo.svg" width={100} height={100} alt="logo" />
+      <Image src="/logo.svg" width={100} height={100} alt="logo" />
     </div>
-  )
-}
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +10,8 @@ const Home: NextPage = () => {
         <title>Sage Fiorentino-Lange</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header />
     </div>
   );
 };
