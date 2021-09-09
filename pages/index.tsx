2312685d16da5d9c782070dd1e1e24a4ad4b9ff7
Sage Fiorentino-Lange <sage.fio.lange@gmail.com>
2021-09-09 13:42:27 -0600

    idk i had to clean up a change still? i think it was vs code being a bitch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-<<<<<<< HEAD
 
 const Home: NextPage = () => {
   return (
@@ -74,20 +73,6 @@ const Home: NextPage = () => {
       aksldjfalsdkjalsdfjalsdjfadsjfasdfadsf
       aksldjfalsdkjalsdfjalsdjfadsjfasdfadsf
     </p>
-=======
-import Header from "../components/Header";
-
-const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Sage Fiorentino-Lange</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Header />
-    </>
->>>>>>> parent of 7f02bd9... i broke it. i think i need to use layout components
   );
 };
 
