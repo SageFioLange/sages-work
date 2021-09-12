df44b3b27a508beb7766dfd85041c02eb15fceb2
Sage Fiorentino-Lange <sage.fio.lange@gmail.com>
2021-09-11 19:23:08 -0600

    i think i finally figured out how to use dynamic imports

---
+++ components/Gallery.tsx
@@ -0,0 +1,55 @@
+import { NextComponentType } from "next";
+import styles from "../styles/Gallery.module.css";
+import { isMobile } from "react-device-detect";
+import Image from "next/image";
+
+type GalleryProps = {
+  content: {
+    name: string;
+    url: string;
+  }[];
+  points: number[][];
+};
+
+const Gallery: NextComponentType<{}, {}, GalleryProps> = ({
+  content,
+  points,
+}: GalleryProps) => {
+  return (
+    <div
+      className={styles.gallery}
+      style={{
+        height: isMobile ? "250vh" : "100vh",
+        width: isMobile ? "100vw" : "250vw",
+      }}
+    >
+      {content.map(({ name, url }, idx) => (
+        <div
+          key={idx}
+          style={{
+            gridColumn: isMobile
+              ? `${points[idx][2]} / span ${points[idx][3]}`
+              : `${points[idx][0]} / span ${points[idx][1]}`,
+            gridRow: isMobile
+              ? `${points[idx][0]} / span ${points[idx][1]}`
+              : `${points[idx][2]} / span ${points[idx][3]}`,
+            placeSelf: "stretch",
+          }}
+          data-scroll
+          data-scroll-speed={`${points[idx][4]}`}
+        >
+          <Image
+            loading="eager"
+            alt={name}
+            src={url}
+            className={styles.image}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Gallery;
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,45 +1,13 @@
-import Image from "next/image";
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 import { homeContent, points } from "../utils/constants";
-import { isMobile } from "react-device-detect";
+import dynamic from "next/dynamic";
+
+const Gallery = dynamic(() => import("../components/Gallery"), {
+  ssr: false,
+});
 
 const Home: NextPage = () => {
-  return (
-    <div
-      className={styles.gallery}
-      style={{
-        height: isMobile ? "250vh" : "100vh",
-        width: isMobile ? "100vw" : "250vw",
-      }}
-    >
-      {homeContent.map(({ name, url }, idx) => (
-        <div
-          key={idx}
-          style={{
-            gridColumn: isMobile
-              ? `${points[idx][2]} / span ${points[idx][3]}`
-              : `${points[idx][0]} / span ${points[idx][1]}`,
-            gridRow: isMobile
-              ? `${points[idx][0]} / span ${points[idx][1]}`
-              : `${points[idx][2]} / span ${points[idx][3]}`,
-            placeSelf: "stretch",
-          }}
-          data-scroll
-          data-scroll-speed={`${points[idx][4]}`}
-        >
-          <Image
-            loading="eager"
-            alt={name}
-            src={url}
-            className={styles.image}
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
-      ))}
-    </div>
-  );
+  return <Gallery content={homeContent} points={points} />;
 };
 
 export default Home;
