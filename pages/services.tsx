56986ff8252aa2a4da9d42594b1e1bc69a973f9a
Sage Fiorentino-Lange <sage.fio.lange@gmail.com>
2021-10-01 13:56:53 -0600

    changed some prices

--- pages/services.tsx
+++ pages/services.tsx
@@ -27,9 +27,11 @@ const Home: NextPage = () => {
         <h3 style={{ gridArea: "1 / 1 / 2 /2" }}>Service</h3>
         <h3 style={{ gridArea: "1 / 2 / 2 / 3" }}>Rate</h3>
         <p style={{ gridArea: "2 / 1 / 3 / 2" }}>
-          Web Development and Search Engine Optimization
+          Web Design and Web Development
         </p>
-        <p style={{ gridArea: "3 / 1 / 4 / 2" }}>Graphic Design and Branding</p>
+        <p style={{ gridArea: "3 / 1 / 4 / 2" }}>
+          Graphic Design and UI/UX Design
+        </p>
         <p style={{ gridArea: "4 / 1 / 5 / 2" }}>
           Music Production, Mixing, and Live Performances
         </p>
@@ -42,14 +44,14 @@ const Home: NextPage = () => {
         <p style={{ gridArea: "5 / 1 / 6 / 2" }}>
           Creative Direction and Project Management
         </p>
-        <p style={{ gridArea: "2 / 2 / 3 / 2" }}>$45/hr | $25/hr</p>
-        <p style={{ gridArea: "3 / 2 / 4 / 2" }}>$25/hr | $35/hr</p>
+        <p style={{ gridArea: "2 / 2 / 3 / 2" }}>$40/hr | $80/hr</p>
+        <p style={{ gridArea: "3 / 2 / 4 / 2" }}>$30/hr | $50/hr</p>
         <p style={{ gridArea: "4 / 2 / 5 / 2" }}>
-          $20/hr | $35/hr | Negotiable
+          $30/hr | $30/hr | Negotiable
         </p>
         {/* <p style={{ gridArea: "5 / 2 / 6 / 2" }}>$90/hr</p>
         <p style={{ gridArea: "6 / 2 / 7 / 2" }}>$50/hr | $30/hr</p> */}
-        <p style={{ gridArea: "5 / 2 / 6 / 2" }}>$55/hr</p>
+        <p style={{ gridArea: "5 / 2 / 6 / 2" }}>$60/hr</p>
       </div>
     </div>
   );
